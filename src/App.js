c989e1145eaccf660e26e4d36099ee89bc424714
maksymt1 <34755191+maksymt1@users.noreply.github.com>
2020-09-04 10:14:01 -0400

    poc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./styles.css";
-import Form from "react-jsonschema-form";
+// import "./styles.css";
+import Form from "react-jsonschema-form-bs4";
 
 let jsonschema = {
   title: "Property dependencies",
@@ -27,21 +27,62 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      jsonschema: jsonschema,
-      uischema: uischema,
-      formdata: formdata
+      jsonSchema: jsonschema,
+      uiSchema: uischema,
+      formData: formdata
     };
   }
+
+  componentDidMount() {
+    console.log("mounted");
+    this.setEnum();
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("updated");
+    console.log(this.state);
+  }
+
+  setEnum() {
+    let schema = this.state.jsonSchema;
+    let data = this.state.formData;
+    for (let p in schema["properties"]) {
+      if (schema["properties"][p]["type"] === "string") {
+        schema["properties"][p]["enum"] = ["mike", "mary", "morty"];
+        data[p] = "";
+      }
+    }
+    // let dataCopy = this.state.formData;
+    this.setState({
+      jsonSchema: schema,
+      formData: data
+    });
+    console.log(this.state);
+  }
+
+  setFormState = (e) => {
+    this.setState({
+      jsonSchema: e.schema,
+      uiSchema: e.uiSchema,
+      formData: e.formData
+    });
+  };
+
+  submit = () => {
+    console.log("Submitted");
+    console.log(this.state);
+  };
 
   render() {
     return (
       <div className="App">
         <Form
-          schema={this.state.jsonschema}
-          uiSchema={this.state.uischema}
-          formData={this.state.formdata}
-          liveValidate={true}
+          schema={this.state.jsonSchema}
+          uiSchema={this.state.uiSchema}
+          formData={this.state.formData}
+          liveValidate={false}
           showErrorList={false}
+          onChange={(e) => this.setFormState(e)}
           onSubmit={this.submit}
         >
           <button type="submit" className="btn btn-primary">
